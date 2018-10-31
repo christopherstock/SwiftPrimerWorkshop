40baeefc63cd7f3f4adc0953550950190b7b23eb
Christopher Stock <email@christopherstock.de>
2018-10-31 10:27:33 +0100

    Initial project setup

---
+++ SwiftPrimerWorkshop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPrimerWorkshop
+//
+//  Created by Christopher Stock on 31.10.18.
+//  Copyright © 2018 Mayflower GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
