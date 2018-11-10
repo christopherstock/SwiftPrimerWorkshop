1fcebe5c914fc6d87733b2094ba64960612c7fad
Christopher Stock <christopher.stock@mayflower.de>
2018-11-10 17:44:32 +0100

    Added action and outlet for Crawl Button.

--- SwiftPrimerWorkshop/de.mayflower.spw/ui/ViewController.swift
+++ SwiftPrimerWorkshop/de.mayflower.spw/ui/ViewController.swift
@@ -2,12 +2,25 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    /** The 'Crawl it!' button. */
+    @IBOutlet weak var crawlButton: UIButton!
 
-
+    /**
+     *  Being invoked when the view controller is fully loaded.
+     */
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         Debug.log( "ViewController.viewDidLoad()" )
+    }
+
+
+    /** Being invoked when the button is pressed. */
+    @IBAction func onPressCrawlButton( _ sender: Any )
+    {
+        Debug.log( "Crawl Button pressed" )
+
+
     }
 }
