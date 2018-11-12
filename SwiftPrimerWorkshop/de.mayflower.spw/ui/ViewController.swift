d5d1cafb306ac9a9f609e9e4b4b18e46b601fefb
Christopher Stock <christopher.stock@mayflower.de>
2018-11-12 15:22:03 +0100

    Added more IB outlets and accessed UI elements.

--- SwiftPrimerWorkshop/de.mayflower.spw/ui/ViewController.swift
+++ SwiftPrimerWorkshop/de.mayflower.spw/ui/ViewController.swift
@@ -2,8 +2,14 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    // * * * Interface Builder Outlets go here.. * * *
+
+    /** The 'URL input' text field. */
+    @IBOutlet weak var urlInputField: UITextField!
     /** The 'Crawl it!' button. */
-    @IBOutlet weak var crawlButton: UIButton!
+    @IBOutlet weak var crawlButton    :UIButton!
+    /** The 'HTML output' text view. */
+    @IBOutlet weak var htmlOutputText :UITextView!
 
     /**
      *  Being invoked when the view controller is fully loaded.
@@ -15,11 +21,23 @@ class ViewController: UIViewController
         Debug.log( "ViewController.viewDidLoad()" )
     }
 
+    // * * * Interface Builder Actions go here.. * * *
 
     /** Being invoked when the 'Crawl it!' button is pressed. */
     @IBAction func onPressCrawlButton( _ sender: Any )
     {
         Debug.log( "Crawl Button pressed" )
+
+        htmlOutputText.text = (
+            "Button pressed at "
+            + DateFormatter().description
+            + "\n"
+            + "Input field text is:\n"
+            + urlInputField.text!
+        )
+
+
+
 
 
     }
